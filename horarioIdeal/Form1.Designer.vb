b46c1e2eaeb47fa2855323a2d3a940f5e8c7e476
santyrgh17 <santiagoguereroh034@gmail.com>
2023-05-12 14:49:50 -0500

    Agregar archivos de proyecto.

---
+++ horarioIdeal/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        GroupBox1 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        TextBox1.Location = New Point(124, 35)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(215, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(128))
+        TextBox2.Location = New Point(136, 84)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(215, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = Color.Navy
+        Label1.Location = New Point(19, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(71, 21)
+        Label1.TabIndex = 2
+        Label1.Text = "Usuario"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI Emoji", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = Color.Navy
+        Label2.Location = New Point(31, 86)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(99, 21)
+        Label2.TabIndex = 3
+        Label2.Text = "Contraseña"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LightSteelBlue
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(250, 135)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Entrar"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.LightSteelBlue
+        Button2.ForeColor = Color.Black
+        Button2.Location = New Point(160, 135)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Finalizar"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.Transparent
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(399, 181)
+        GroupBox1.TabIndex = 6
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Login"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(GroupBox1)
+        DoubleBuffered = True
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+End Class
---
+++ horarioIdeal/frm_principal.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_principal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        AdministrarToolStripMenuItem = New ToolStripMenuItem()
+        UsuariosToolStripMenuItem = New ToolStripMenuItem()
+        RolesToolStripMenuItem = New ToolStripMenuItem()
+        AccionesToolStripMenuItem = New ToolStripMenuItem()
+        PermisosToolStripMenuItem = New ToolStripMenuItem()
+        ParametrosToolStripMenuItem = New ToolStripMenuItem()
+        SalirToolStripMenuItem = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {AdministrarToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' AdministrarToolStripMenuItem
+        ' 
+        AdministrarToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {UsuariosToolStripMenuItem, RolesToolStripMenuItem, AccionesToolStripMenuItem, PermisosToolStripMenuItem, ParametrosToolStripMenuItem, SalirToolStripMenuItem})
+        AdministrarToolStripMenuItem.Name = "AdministrarToolStripMenuItem"
+        AdministrarToolStripMenuItem.Size = New Size(81, 20)
+        AdministrarToolStripMenuItem.Text = "Administrar"
+        ' 
+        ' UsuariosToolStripMenuItem
+        ' 
+        UsuariosToolStripMenuItem.Name = "UsuariosToolStripMenuItem"
+        UsuariosToolStripMenuItem.Size = New Size(134, 22)
+        UsuariosToolStripMenuItem.Text = "Usuarios"
+        ' 
+        ' RolesToolStripMenuItem
+        ' 
+        RolesToolStripMenuItem.Name = "RolesToolStripMenuItem"
+        RolesToolStripMenuItem.Size = New Size(134, 22)
+        RolesToolStripMenuItem.Text = "Roles"
+        ' 
+        ' AccionesToolStripMenuItem
+        ' 
+        AccionesToolStripMenuItem.Name = "AccionesToolStripMenuItem"
+        AccionesToolStripMenuItem.Size = New Size(134, 22)
+        AccionesToolStripMenuItem.Text = "Acciones"
+        ' 
+        ' PermisosToolStripMenuItem
+        ' 
+        PermisosToolStripMenuItem.Name = "PermisosToolStripMenuItem"
+        PermisosToolStripMenuItem.Size = New Size(134, 22)
+        PermisosToolStripMenuItem.Text = "Permisos"
+        ' 
+        ' ParametrosToolStripMenuItem
+        ' 
+        ParametrosToolStripMenuItem.Name = "ParametrosToolStripMenuItem"
+        ParametrosToolStripMenuItem.Size = New Size(134, 22)
+        ParametrosToolStripMenuItem.Text = "Parametros"
+        ' 
+        ' SalirToolStripMenuItem
+        ' 
+        SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        SalirToolStripMenuItem.Size = New Size(134, 22)
+        SalirToolStripMenuItem.Text = "Salir"
+        ' 
+        ' frm_principal
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "frm_principal"
+        Text = "frm_principal"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AdministrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UsuariosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RolesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccionesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PermisosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ParametrosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+End Class
